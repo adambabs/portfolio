7af51f1d374f98c7670aa9d1d20aa21401a009a2
adambabs <adam.babs@ing.com>
2023-06-29 11:45:24 +0200

    visual changes

--- pages/components/Hero.tsx
+++ pages/components/Hero.tsx
@@ -1,3 +1,42 @@
+// import React from "react";
+// import config from "../index.json";
+
+// const Hero = () => {
+//   const hero = config.hero;
+
+//   return (
+//     <section className="h-screen bg-white overflow-x-hidden">
+//       <div className="px-8 lg:px-32 flex flex-col justify-center align-between">
+//         <h1 className="text-6xl mt-64 font-bold tracking-wide">
+//           Hi, my name is  
+//           <span className="ml-3 whitespace-nowrap bg-gradient-to-r from-indigo-900 via-indigo-600 to-white bg-clip-text text-transparent">
+//             {hero.name}
+//           </span>
+//         </h1>
+//         <h1 className="text-6xl font-bold tracking-wide mt-4">{hero.subtitle}</h1>
+//         <a href="#About" className="text-2xl font-bold p-0.5 mt-6 w-44 bg-gradient-to-r from-indigo-900 via-indigo-600 to-white-100">
+//           <div className="bg-white">
+//             <span className="block text-center py-0.5 px-2 font-semibold bg-white font-bold bg-gradient-to-r from-indigo-900 via-indigo-600 to-white-100 bg-clip-text text-transparent">
+//               Know more
+//             </span>
+//           </div>
+//         </a>
+//         {/* <div className="flex justify-start mt- ml-">
+//           <a href="https://www.linkedin.com/in/your-profile-url" target="_blank" rel="noreferrer">
+//             <img src="/img/linkedinlogo.png" alt="LinkedIn Logo" className="w-6 h-6 inline-block" />
+//           </a>
+//         </div> */}
+//         <div className="flex justify-center">
+//           <a href="https://portfolio-adambabs.vercel.app/" target="_blank" rel="noreferrer">
+//             <img src="/img/me.jpg" alt="Profile Photo" className="mt-8 rounded-full w-80 h-80 object-cover" />
+//           </a>
+//         </div>
+//       </div>
+//     </section>
+//   );
+// };
+
+// export default Hero;
 import React from "react";
 import config from "../index.json";
 
@@ -21,14 +60,18 @@ const Hero = () => {
             </span>
           </div>
         </a>
-        {/* <div className="flex justify-start mt- ml-">
-          <a href="https://www.linkedin.com/in/your-profile-url" target="_blank" rel="noreferrer">
-            <img src="/img/linkedinlogo.png" alt="LinkedIn Logo" className="w-6 h-6 inline-block" />
-          </a>
-        </div> */}
-        <div className="flex justify-center">
+        <div className="flex justify-center mt-4">
           <a href="https://portfolio-adambabs.vercel.app/" target="_blank" rel="noreferrer">
             <img src="/img/me.jpg" alt="Profile Photo" className="mt-8 rounded-full w-80 h-80 object-cover" />
+          </a>
+        </div>
+        {/* LinkedIn and GitHub logos */}
+        <div className="flex justify-center mt-4">
+          <a href="https://www.linkedin.com/in/adam-babs-76124415b/" target="_blank" rel="noreferrer" className="mr-2">
+            <img src="/img/linkedinlogo.png" alt="LinkedIn Logo" className="w-6 h-6 inline-block" />
+          </a>
+          <a href="https://github.com/adambabs" target="_blank" rel="noreferrer">
+            <img src="/img/githublogo.png" alt="GitHub Logo" className="w-7 h-7 inline-block" />
           </a>
         </div>
       </div>
@@ -37,4 +80,3 @@ const Hero = () => {
 };
 
 export default Hero;
-
